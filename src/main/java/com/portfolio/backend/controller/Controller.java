260c28a8a8d36aeaf53c6f35d2ac412d4a21c0e4
Mario Adrian Turk <ma_turk@yahoo.com>
2022-07-28 22:44:22 -0300

    @crossOrigin added

--- src/main/java/com/portfolio/backend/controller/Controller.java
+++ src/main/java/com/portfolio/backend/controller/Controller.java
@@ -5,6 +5,7 @@ import com.portfolio.backend.interfaces.IUserService;
 import com.portfolio.backend.model.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class Controller {
  
     private static final String USERS_GET_PATH = "users/get";
